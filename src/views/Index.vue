d8fea3d7396aec149a07f6ef862c29e32b77a979
DESKTOP-OE6BT01\strong <710427941@qq.com>
2018-04-27 00:33:14 +0800

    [第一次提交]已配置好所有的vue-cli，准备开始开发首页

---
+++ src/views/Index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="index-header">
+        <van-row>
+            <van-col span="5">span3</van-col>
+            <van-col span="14">span18</van-col>
+            <van-col span="5">span3</van-col>
+        </van-row>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
