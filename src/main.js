fa3049ce6e399d1ea8a71b432cbba0292d183d5d
DESKTOP-8IANAK9\strong <710427941@qq.com>
2018-07-22 22:05:32 +0800

    [完善]注册页面

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import {Lazyload,List,Row,Col,Button} from 'vant'
+import { Lazyload, List, Row, Col, Button, Field, NavBar } from 'vant'
 
 
-Vue.use(Lazyload).use(List).use(Row).use(Col).use(Button)
+Vue.use(Lazyload).use(List).use(Row).use(Col).use(Button).use(Field).use(NavBar)
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  render: h => h(App)
+    el: '#app',
+    router,
+    render: h => h(App)
 })
