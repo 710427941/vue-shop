ca1cfacce4a7c77f90e01eb22ad6dad3efb38ec8
DESKTOP-8IANAK9\strong <710427941@qq.com>
2018-07-29 23:14:53 +0800

    [完善]商品详情

--- src/main.js
+++ src/main.js
@@ -1,10 +1,24 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import { Lazyload, List, Row, Col, Button, Field, NavBar, Swipe, SwipeItem } from 'vant'
+import { Lazyload, List, Row, Col, Button, Field, NavBar, Swipe, SwipeItem, GoodsAction, GoodsActionBigBtn, GoodsActionMiniBtn, Tab, Tabs } from 'vant'
 
 
-Vue.use(Lazyload).use(List).use(Row).use(Col).use(Button).use(Field).use(NavBar).use(Swipe).use(SwipeItem)
+Vue
+    .use(Tab)
+    .use(Tabs)
+    .use(Lazyload)
+    .use(List)
+    .use(Row)
+    .use(Col)
+    .use(Button)
+    .use(Field)
+    .use(NavBar)
+    .use(Swipe)
+    .use(SwipeItem)
+    .use(GoodsAction)
+    .use(GoodsActionBigBtn)
+    .use(GoodsActionMiniBtn)
 
 Vue.config.productionTip = false
 
--- src/views/GoodInfo.vue
+++ src/views/GoodInfo.vue
@@ -6,12 +6,42 @@
             left-arrow
             @click-left = "goBack"
         />
-        <van-swipe :autoplay="3000">
-            <van-swipe-item v-for="(image, index) in images" :key="index">
-                <img v-lazy="image" width="100%"/>
-            </van-swipe-item>
-           
-        </van-swipe>
+
+        <div class="goodsinfobanner">
+            <van-swipe :autoplay="3000">
+                <van-swipe-item v-for="(image, index) in images" :key="index">
+                    <img v-lazy="image" width="100%"/>
+                </van-swipe-item>
+            </van-swipe>
+        </div>
+        
+        <div class="footer">
+            <van-goods-action>
+                <van-goods-action-mini-btn icon="chat" text="客服" />
+                <van-goods-action-mini-btn icon="cart" text="购物车" info="5"/>
+                <van-goods-action-big-btn text="加入购物车" />
+                <van-goods-action-big-btn text="立即购买" primary />
+            </van-goods-action>
+        </div>
+
+        <div class="goods-name">
+            {{goodsInfo.NAME}}
+        </div>
+        <div class="goods-price">
+            {{goodsInfo.ORI_PRICE | money}}
+        </div>
+
+        <div class="goodsinfo">
+            <van-tabs sticky swipeable>
+                <van-tab title='详情'>
+                    <div class="detatl" v-html="goodsInfo.DETAIL"></div>
+                </van-tab>
+                <van-tab title='评论'>
+                    评论开发中....
+                </van-tab>
+            </van-tabs>
+        </div>
+
     </div>
 </template>
 
@@ -19,11 +49,13 @@
     import axios from 'axios'
     import url from '@/serviceApi.config.js'
     import { Toast } from 'vant'
+    import {toMoney} from '@/filter/moneyFilter.js';
     export default {
         data() {
             return {
                 goodsId:'',
-                images:[]
+                images:[],
+                goodsInfo:{}
             }
         },
         created(){
@@ -41,7 +73,9 @@
                 }).
                 then(response=>{
                     if(response.data.code ==200 && response.data.message){
+                        console.log(response.data.message)
                         this.images.push(response.data.message.IMAGE1) 
+                        this.goodsInfo = response.data.message
                     }else{
                         Toast('数据获取异常')
                         setTimeout(()=>{
@@ -61,9 +95,19 @@
                 this.$router.go(-1)
             }
         },
+        filters:{
+            money(m){
+               return toMoney(m)
+            }
+        }
     }
 </script>
 
 <style scoped>
-
+    .goods-name{background-color: #fff;font-size: 1rem;padding: 20px 10px;border-bottom: 1px solid #f1f1f1;}
+    .goods-price{color: #f00;background-color: #fff;padding: 20px 10px}
+    .detatl {font-size: 0;}
+    .goodsinfo{padding-bottom: 50px;}
+    .footer{position: fixed;z-index: 99;left: 0;bottom: 0;width: 100%}
+    .goodsinfobanner{background-color: #fff;border-bottom: 1px solid #f1f1f1}
 </style>
